1adb01447afc837732ee57030a5bc33588004627
Qurratulain7 <122641606+Qurratulain7@users.noreply.github.com>
2023-08-24 09:44:00 +0530

    Add files via upload
    added files

---
+++ main.js
@@ -0,0 +1,19 @@
+let name2 = "admin"
+let num1 =22
+let num2 =33
+let sum 
+sum = num1 + num2
+console.log(name2)
+console.log(sum)
+
+
+document.getElementById("btn").onauxclick = function(){
+ console.log
+
+
+
+
+
+
+
+}
